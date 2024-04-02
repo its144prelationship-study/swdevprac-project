baa63d115cc39a4e3cd61c77951d95d309494dc2
Thanach Silapatcharanun <111685309+Porsche-ths@users.noreply.github.com>
2024-04-02 16:24:50 +0700

    feat: init company model

---
+++ models/Company.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema({
+    company_name: {
+        type: String,
+        required: [true, "Company name is required"],
+    },
+    address: {
+        type: String,
+        required: [true, "Address is required"],
+    },
+    website: {
+        type: String,
+        required: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    tel: {
+        type: String,
+        required: [true, "Telephone number is required"],
+        maxlength: [10, "Telephone number must not exceed 10 digits"],
+    },
+    receiving_pos: {
+        type: [{ type: String }],
+        required: false,
+    },
+});
+
+module.exports = mongoose.model("Companies", companySchema);
