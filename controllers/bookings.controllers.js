00d2031a85d11c7837a79ff1751a2f9bfff8ae4c
its144prelationship <pastalonely@gmail.com>
2024-04-04 17:02:35 +0700

    feat: init booking controller function

---
+++ controllers/bookings.controllers.js
@@ -0,0 +1,14 @@
+const Booking = require("../models/Booking.model");
+const Company = require("../models/Company.model");
+
+exports.getBookings = async (req, res, next) => {};
+
+exports.getCompanyBookings = async (req, res, next) => {};
+
+exports.getBooking = async (req, res, next) => {};
+
+exports.createBooking = async (req, res, next) => {};
+
+exports.updateBooking = async (req, res, next) => {};
+
+exports.deleteBooking = async (req, res, next) => {};
