fe0eec5a9def118c24f293d943d0c9c26f09e8a6
its144prelationship <pastalonely@gmail.com>
2024-04-04 17:01:51 +0700

    feat: add booking route

--- routes/companies.routes.js
+++ routes/companies.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { getCompanies, getCompany, createCompany, updateCompany, deleteCompany } = require("../controllers/companies.controllers");
+const bookingRouter = require("./bookings.routes");
 const favoriteRouter = require("./favorites.routes");
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth.middleware");
 
+router.use("/:companyId/bookings", bookingRouter);
 router.use("/:companyId/favorites", favoriteRouter);
 
 router.route("/").get(getCompanies).post(createCompany);
