546daf39bde628f4674e8eea062668864398df64
Thanach Silapatcharanun <111685309+Porsche-ths@users.noreply.github.com>
2024-04-04 18:16:28 +0700

    feat: add middleware to favorite api

--- routes/favorites.routes.js
+++ routes/favorites.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { getFavorites, createFavorite, deleteFavorite } = require("../controllers/favorites.controllers");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const { protect } = require("../middleware/auth.middleware");
 
-router.route("/").get(getFavorites).post(createFavorite);
-router.route("/:favoriteId").delete(deleteFavorite);
+router.route("/").get(protect, getFavorites).post(protect, createFavorite);
+router.route("/:favoriteId").delete(protect, deleteFavorite);
 
 module.exports = router;
