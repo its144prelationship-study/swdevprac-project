6e5898fb2a18a7d748fb75716daff09e2139c20c
Thanach Silapatcharanun <111685309+Porsche-ths@users.noreply.github.com>
2024-04-09 02:18:31 +0700

    feat: add API security

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -15,6 +21,17 @@ const favorites = require("./routes/favorites.routes");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(hpp());
+app.use(cors());
 
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/companies", companies);
